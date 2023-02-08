0e56f10f689614a8e0b72d48b58b09d7b44100b6
unknown <partharora557@gmail.com>
2023-02-08 06:50:34 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { ImagePanorama } from 'panolens';
 function App() {
+
+  const viewer = new ImagePanorama();
+  React.useEffect(()=>{
+    const imgContainer = document.getElementsByClassName("Image-container");
+    console.log(imgContainer[0]);
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' >
+      <div className="Image-container" >
+
+      </div>
     </div>
   );
 }
