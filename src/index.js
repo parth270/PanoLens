6cbeddf45c698b56defcce5169c863d4b32b0faf
unknown <partharora557@gmail.com>
2023-02-08 07:59:18 +0530

    final-commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import First from './pages/first';
+import Second from './pages/second';
+import Third from './pages/third';
+import Fourth from './pages/fourth';
+import Fifth from './pages/fifth';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+const ChangeRoute=()=>{
+  const navigate = useNavigate();
+  React.useEffect(()=>{
+    const randomNumber= Math.floor(Math.random()*5)+1;
+    navigate(`/${randomNumber}`);
+  })
+  return(
+    <>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<ChangeRoute/>
+  },
+  {
+    path: "1",
+    element: <First />
+  },
+  {
+    path: "2",
+    element: <Second/>,
+  },
+  {
+    path: "3",
+    element:<Third/>,
+  },
+  {
+    path: "4",
+    element: <Fourth/>,
+  },
+  {
+    path: "5",
+    element: <Fifth/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
---
+++ src/pages/third.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ImagePanorama, Viewer } from "panolens";
+import { useNavigate } from 'react-router-dom';
+function App() {
+    const arr = [
+        "./n3 (1).jpeg",
+        "./n3 (2).jpeg",
+        "./n3 (3).jpeg",
+        "./n3 (4).jpeg",
+        "./n3 (5).jpeg",
+    ];
+  React.useEffect(() => {
+      const imgContainer = document.getElementsByClassName("Image-container");
+      const img = new ImagePanorama(arr[1]);
+      const viewer = new Viewer({
+          container: imgContainer[0],
+        });
+        viewer.add(img);
+    });
+    
+    const navigate = useNavigate();
+      const clicked=(e)=>{
+          e.preventDefault();
+          const randomNumber= Math.floor(Math.random()*5)+1;
+          navigate(`/${randomNumber}`)
+      }
+  return (
+    <div className="container">
+      <div className="custom-container">
+        <h1>Random image?</h1>
+        <button id="btn" onClick={clicked} >Change?</button>
+      </div>
+      <div className="Image-container"></div>
+    </div>
+  );
+}
+
+export default App;
